1ee218dc92c8c5052e1dfe4767b6dc3cb31112e3
Moritz Niklas Kluwe <moritz-niklas.kluwe@imfd.tu-freiberg.de>
2024-08-08 09:06:03 +0200

    Added docstrings, changed container for single camera calibration from list to set in multi camera object

--- laser_cross_detection/calibration/soloff_polynom.py
+++ laser_cross_detection/calibration/soloff_polynom.py
@@ -61,6 +61,19 @@ def make_nd_polynom(*orders: int) -> List[Tuple[str]]:
 
 @dataclass
 class SoloffPolynom:
+    """Container representing Soloff polynomials and methods used to fit
+    the coefficients. The Soloff polynomial can be used to create a mapping
+    between a single image coordinate and world (x, y, z) coordinates.
+
+    Raises:
+        ValueError: Is raised when the inputs do not comply with the
+            expected shape, e.g.: x, y, z or np.array([x, y, z])
+            or np.array([[x], [y], [z]]) in the __call__ method
+
+    Returns:
+        _type_: SoloffPolynomial
+    """
+
     x_order: int
     y_order: int
     z_order: int
@@ -77,6 +90,19 @@ class SoloffPolynom:
     def __call__(
         self, *args: nptyping.NDArray[np.float64]
     ) -> nptyping.NDArray[np.float64]:
+        """Calculates the pixel coordinates (u, v) from a point in space
+        (x, y, z). The x, y, z coordinates are expected to be passed
+        individually or in a single numpy array.
+
+        Raises:
+            ValueError: Is raised when the inputs do not comply with the
+            expected shape, e.g.: x, y, z or np.array([x, y, z])
+            or np.array([[x], [y], [z]])
+
+        Returns:
+            nptyping.NDArray[np.float64]: Pixel coordinates computed by
+            the underlying Soloff polynomial.
+        """
         if len(args) == 3:
             M = self.build_m(*args)
         elif len(args) == 1:
@@ -93,7 +119,19 @@ class SoloffPolynom:
         self,
         xyz: nptyping.NDArray[np.float64],
         u: nptyping.NDArray[np.float64],
-    ) -> nptyping.NDArray[np.float64]:
+    ):
+        """Fits the parameter of the Soloff polynomial using the
+        scipy.optimize.curve_fit function.
+
+        Args:
+            xyz (nptyping.NDArray[np.float64]): xyz world coordinates of
+                calibration points
+            u (nptyping.NDArray[np.float64]): u image coordinates of
+                calibration points
+
+        Returns:
+            self: returns the instance
+        """
         popt, pcov = sopt.curve_fit(
             self.fn_opt, xyz, u, p0=self.a, method="lm"
         )
@@ -104,13 +142,31 @@ class SoloffPolynom:
         self,
         xyz: nptyping.NDArray[np.float64],
         u: nptyping.NDArray[np.float64],
-    ) -> nptyping.NDArray[np.float64]:
+    ):
+        """Fits the parameter of the Soloff polynomial using the
+        scipy.optimize.least_squares function.
+
+        Args:
+            xyz (nptyping.NDArray[np.float64]): xyz world coordinates of
+                calibration points
+            u (nptyping.NDArray[np.float64]): u image coordinates of
+                calibration points
+
+        Returns:
+            self: returns the instance
+        """
         result = sopt.least_squares(self.fn_opt_ls(xyz, u), x0=self.a)
         self.a = np.asarray(result.x)
         return self
 
     @functools.cached_property
     def polynom(self) -> List[Tuple[str]]:
+        """Definition of the variable coefficients of the Soloff respective
+        polynmial.
+
+        Returns:
+            List[Tuple[str]]: variable coefficients of the Soloff polynomial
+        """
         return make_nd_polynom(self.x_order, self.y_order, self.z_order)
 
     def fn_opt_ls(
@@ -118,6 +174,19 @@ class SoloffPolynom:
         xyz: nptyping.NDArray[np.float64],
         u: nptyping.NDArray[np.float64],
     ) -> Callable:
+        """Builds the optimization function used for least_squares fit of
+        polynomial coefficients.
+
+        Args:
+            xyz (nptyping.NDArray[np.float64]): xyz world coordinates of
+                calibration points
+            u (nptyping.NDArray[np.float64]): u image coordinates of
+                calibration points
+
+        Returns:
+            Callable: function used for least_squares fit
+        """
+
         def f(a):
             s = self.fn_opt(xyz, *a)
             return s - u
@@ -127,6 +196,17 @@ class SoloffPolynom:
     def fn_opt(
         self, xyz: nptyping.NDArray[np.float64], *a: float
     ) -> nptyping.NDArray[np.float64]:
+        """Optimization function used for scipy.optimize.curve_fit to fit
+        the polynomial coefficients.
+
+        Args:
+            xyz (nptyping.NDArray[np.float64]): xyz world coordinates of
+                calibration points
+            a (float): parameters of the Soloff polynomial
+
+        Returns:
+            nptyping.NDArray[np.float64]: u image coordinates
+        """
         self.a = np.asarray(a)
         return self.__call__(xyz)
 
@@ -136,6 +216,18 @@ class SoloffPolynom:
         x2: nptyping.NDArray[np.float64],
         x3: nptyping.NDArray[np.float64],
     ) -> nptyping.NDArray[np.float64]:
+        """Builds the variable coefficient matrix of the Soloff polynomial
+        using actual coordinates x1, x2, x3 (x, y, z).
+
+        Args:
+            x1 (nptyping.NDArray[np.float64]): first world coordinate
+            x2 (nptyping.NDArray[np.float64]): second world coordinate
+            x3 (nptyping.NDArray[np.float64]): third world coordinate
+
+        Returns:
+            nptyping.NDArray[np.float64]: matrix of variable coefficients
+                according to underlying Soloff polynomial
+        """
         mapping = {"x1": x1, "x2": x2, "x3": x3}
         m = [
             np.prod([mapping[param] for param in params], axis=0)
