8565a99d11d4b96ccbafbdd0d4fd7ce85134682d
Moritz Niklas Kluwe <moritz-niklas.kluwe@imfd.tu-freiberg.de>
2026-01-21 09:39:52 +0100

    test: add test for kluwe and utility method
    - add a test for accurate angle calculation
    - add a function to estimate angle between two line orientations
    repsecting opposite orientation and wrap around
    - add image size as hypothesis parameter, eg. testing now different
    image sizes

--- tests/test_kluwe.py
+++ tests/test_kluwe.py
@@ -21,6 +21,20 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
+def angle_diff_mod_pi(a: float, b: float) -> float:
+    """Orientation distance (theta = theta + n pi)
+
+    Args:
+        a - first angle in radians
+        b - second angle in radians
+    Reutrns:
+        angle between both angles
+    """
+    z1 = np.exp(2j * a)
+    z2 = np.exp(2j * b)
+    return np.abs(np.angle(z1 * np.conj(z2))) / 2.0
+
+
 @dataclass
 class GeneratedTestCase:
     config: ImageConfig
@@ -31,7 +45,11 @@ class GeneratedTestCase:
 
 
 @st.composite
-def beam_image_noise_free(draw, dimension: tuple[int, int] = (800, 800)):
+def beam_image_noise_free(draw):
+    # draw image dimension
+    width = draw(st.integers(min_value=250, max_value=2000))
+    height = draw(st.integers(min_value=250, max_value=2000))
+
     # draw angles with minimum separation
     angle1 = draw(st.floats(min_value=0.0, max_value=180, exclude_max=True))
     offset = draw(st.floats(min_value=20, max_value=160))
@@ -47,15 +65,15 @@ def beam_image_noise_free(draw, dimension: tuple[int, int] = (800, 800)):
     dtype = draw(st.sampled_from(["uint8", "uint16", "float32", "float64"]))
 
     # distace
-    distance1 = draw(st.floats(min_value=0.0, max_value=min(dimension) / 2.0))
-    distance2 = draw(st.floats(min_value=0.0, max_value=min(dimension) / 2.0))
+    distance1 = draw(st.floats(min_value=0.0, max_value=min((width, height)) / 2.0))
+    distance2 = draw(st.floats(min_value=0.0, max_value=min((width, height)) / 2.0))
 
     # build config
     line1 = ComplexHessLine.from_degrees(angle_deg=angle1, distance=distance1)
     line2 = ComplexHessLine.from_degrees(angle_deg=angle2, distance=distance2)
 
     config = ImageConfig(
-        dimension=ImageDimension(*dimension),
+        dimension=ImageDimension(width=width, height=height),
         beams=[
             BeamConfig(
                 angle=line1.angle_deg,
@@ -77,10 +95,12 @@ def beam_image_noise_free(draw, dimension: tuple[int, int] = (800, 800)):
     image = SyntheticImageGenerator(config).generate()
 
     # compute expected angles in detector frame
-    expected_angles = np.sort((90 - np.array([angle1, angle2])) % 180)
+    expected_angles = np.sort((90.0 - np.array([angle1, angle2])) % 180.0)
 
     # compute expected point of intersection
-    expected_intersection = np.array(line1.intersect(line2)) + np.array(dimension) / 2
+    expected_intersection = (
+        np.array(line1.intersect(line2)) + np.array((width, height)) / 2.0
+    )
     return GeneratedTestCase(
         config=config,
         image=image,
@@ -108,7 +128,8 @@ class TestDetectionMethodKluwe:
     def test_image(self, test_case: GeneratedTestCase):
         detector = Kluwe()
         prediction = detector(test_case.image)
-        assert_allclose(prediction, test_case.expected_intersection, rtol=0.02)
+        distance = np.linalg.norm(prediction - test_case.expected_intersection)
+        assert_allclose(distance, 0.0, atol=2.0)  # absolute difference in pixel
 
     @settings(deadline=2000)
     @given(beam_image_noise_free().filter(is_intersection_inside_image))
@@ -117,10 +138,38 @@ class TestDetectionMethodKluwe:
         detector = Kluwe()
         guess, properties = detector.guess_angles(test_case.image)
         guess = guess[np.argsort(properties["prominences"])[-2:]]
-        prediction = np.sort(guess % 180)
+        prediction = np.sort(guess % 180.0)
+
+        diff1 = angle_diff_mod_pi(
+            np.deg2rad(prediction[0]), np.deg2rad(test_case.expected_angles[0])
+        )
+        diff2 = angle_diff_mod_pi(
+            np.deg2rad(prediction[1]), np.deg2rad(test_case.expected_angles[1])
+        )
         # absolute tolerance of 2 degree
         assert_allclose(
-            np.cos(np.deg2rad(2 * prediction)),
-            np.cos(np.deg2rad(2 * test_case.expected_angles)),
-            atol=2,
+            (diff1, diff2),
+            0.0,
+            atol=np.deg2rad(2.5),
+        )
+
+    @settings(deadline=2000)
+    @given(beam_image_noise_free().filter(is_intersection_inside_image))
+    def test_angles_calc(self, test_case: GeneratedTestCase):
+        """Test if the calculated angles are accurate."""
+        detector = Kluwe()
+        angles = detector.calc_angles(test_case.image, method="Powell")
+        prediction = np.sort(angles % 180.0)
+
+        diff1 = angle_diff_mod_pi(
+            np.deg2rad(prediction[0]), np.deg2rad(test_case.expected_angles[0])
+        )
+        diff2 = angle_diff_mod_pi(
+            np.deg2rad(prediction[1]), np.deg2rad(test_case.expected_angles[1])
+        )
+        # absolute tolerance of 1 degree
+        assert_allclose(
+            (diff1, diff2),
+            0.0,
+            atol=np.deg2rad(1.0),
         )
